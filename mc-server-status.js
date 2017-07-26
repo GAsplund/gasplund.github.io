e7544ca61d329d4266bcd8a8e739c91b4f8869c5
Gabriel <Mrgts411@users.noreply.github.com>
2017-07-26 11:34:57 +0300

    Update mc-server-status.js

--- mc-server-status.js
+++ mc-server-status.js
@@ -1,66 +1,79 @@
 var rq = 'https://mcapi.us/server/status?ip=gasplund.mc-server.net';
+
 var error = 'unknown';
+
 var classes = {
-error: "fa-question",
-false: "fa-times",
-true: "fa-check",
-success: "fa-check",
+	error: "fa-question",
+	false: "fa-times",
+	true: "fa-check",
 };
+
 var allclasses = "";
-for(i in classes) {
-allclasses += ' '+classes[i];
+	for(i in classes) {
+	allclasses += ' '+classes[i];
 };
+
 function q(addr, cb) {
-$.ajax({
-url: rq,
-type: 'GET',
-dataType: 'json',
-data: {ip: addr, players: true},
+	$.ajax({
+	url: rq,
+	type: 'GET',
+	dataType: 'json',
+	data: {ip: addr, players: true},
 })
+
 .done(function(data) {
-console.log("success");
-console.log(data);
-cb(data);
+	console.log("success");
+	console.log(data);
+	cb(data);
+	cb("success");
 })
+
 .fail(function(data) {
-console.log("error");
+	console.log("error");
 })
+
 .always(function() {
-});
-}
+});}
+
 function setclass(o, c) {
-o.removeClass(allclasses);
-o.addClass(classes[c]);
-o.html('');
+	o.removeClass(allclasses);
+	o.addClass(classes[c]);
+	o.html('');
 }
+
 function settext(o, t) {
-o.removeClass(allclasses);
-o.html(t);
+	o.removeClass(allclasses);
+	o.html(t);
 }
-function display(data) {
-var
-np = $('#players'),
-version = $('#version'),
-online = $('#online'),
-motd = $('#motd'),
-updated = $('#updated'),
-status = $('#status'),
-d = new Date(data.last_updated*1000);
-moment.locale('*');
-settext(updated, moment(d).fromNow());
-setclass(online, data.online);
-if (data.online) {
-settext(np, data.players.now);
-settext(version, data.server.name);
-settext(motd, data.motd);
-settext(status, data.status);
-} else {
-setclass(np, error);
-setclass(version, error);
-setclass(motd, error);
-setclass(status, error);
+
+function display(data, "success") {
+	var
+		np = $('#players'),
+		version = $('#version'),
+		online = $('#online'),
+		motd = $('#motd'),
+		updated = $('#updated'),
+		status = $('#status'),
+		d = new Date(data.last_updated*1000);
+		moment.locale('*');
+		settext(updated, moment(d).fromNow());
+		setclass(online, data.online);
+
+	if (data.online) {
+		settext(np, data.players.now);
+		settext(version, data.server.name);
+		settext(motd, data.motd);
+		settext(status, "success".status);
+	}
+
+	else {
+		setclass(np, error);
+		setclass(version, error);
+		setclass(motd, error);
+		setclass(status, error);
+	}
 }
-}
+
 $(document).ready(function() {
-q('//lentium.xyz', display);
+	q('//lentium.xyz', display);
 });
