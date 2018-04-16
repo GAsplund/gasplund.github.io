538adb48af398bc20f293d193d66bdd3fdd6a368
xAphex <5391237+xAphex@users.noreply.github.com>
2018-04-16 22:08:29 +0300

    Update ARK Server Checker (test 1)

--- files/ark-server-status.js
+++ files/ark-server-status.js
@@ -1,82 +1,75 @@
 var rq = 'https://ark-servers.net/api/?object=servers&element=detail&key=UGkfgK5PNgpdgwGmGHKGcUgflPIjqIOE9M2';
+var rr = ''
 
 var error = 'unknown';
 
 var classes = {
-	error: "fa-question",
-	0: "fa-times",
-	1: "fa-check",
+    error: "fa-question",
+    0: "fa-times",
+    1: "fa-check",
 };
 
 var allclasses = "";
 
 for(i in classes) {
-	allclasses += ' '+classes[i];
+    allclasses += ' '+classes[i];
 };
 
 function q(addr, cb) {
-	$.ajax({
-		url: rq,
-		type: 'GET',
-		dataType: 'json',
-		data: {ip: addr, players: true},
-	})
+    $.ajax({
+        url: rq,
+        type: 'GET',
+        dataType: 'json',
+        data: {ip: addr, players: true},
+    })
 
-	.done(function(data) {
-		console.log(data);
-		cb(data);
-	})
+    .done(function(data) {
+        console.log(data);
+        cb(data);
+    })
 
-	.fail(function(data) {
-		console.log("error");
-	})
+    .fail(function(data) {
+        console.log("error");
+    })
 
-	.always(function() {
-	});
+    .always(function() {
+    });
 }
 
 function setclass(o, c) {
-	o.removeClass(allclasses);
-	o.addClass(classes[c]);
-	o.html('');
+    o.removeClass(allclasses);
+    o.addClass(classes[c]);
+    o.html('');
 }
 
 function settext(o, t) {
-	o.removeClass(allclasses);
-	o.html(t);
+    o.removeClass(allclasses);
+    o.html(t);
 }
 
 function display(data) {
-	var
-		is_online = $('#online'),
-		players = $('#players'),
-		maxplayers = $('#maxplayers'),
-		version = $('#version'),
-	    	map = $('#map'),
-		last_check = $('#lastcheck'),
-		updated = $('#updated'),
-		d = new Date(data.last_updated*1000);
-		moment.locale('*');
-		settext(updated, moment(d).fromNow());
-		setclass(is_online, data.is_online);
+    var
+        is_online = $('#online'),
+        players = $('#players'),
+        maxplayers = $('#maxplayers'),
+        version = $('#version'),
+        map = $('#map'),
+        lastcheck = $('#last_check'),
+        updated = $('#updated')
+        settext(updated, lastcheck);
+        setclass(is_online, data.is_online);
 
-	if (data.is_online) {
-		settext(players, data.players);
-		settext(maxplayers, data.maxplayers);
-		settext(version, data.version);
-		settext(map, data.map);
-		settext(last_check, data.last_check);
-	}
+    if (data.is_online) {
+        /* Add the corresponding line data */
+        settext(players, data.players);
+        settext(maxplayers, data.maxplayers);
+        settext(version, data.version);
+        settext(map, data.map);
+        settext(last_check, data.last_check);
 
-	else {
-		setclass(players, error);
-		setclass(maxplayers, error);
-		setclass(version, error);
-		setclass(map, error);
-		setclass(last_check, error);
-	}
+    }
+
+    else {
+        /* Just dont add the data I guess */
+    }
 }
-
-$(document).ready(function() {
-	q('//lentium.xyz', display);
-});
