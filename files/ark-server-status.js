212728900f17189a2735dc9d69665179bf5c130f
xAphex <5391237+xAphex@users.noreply.github.com>
2018-04-17 19:01:33 +0300

    Update ARK Server Checker

--- files/ark-server-status.js
+++ files/ark-server-status.js
@@ -1,38 +1,38 @@
-var rq = 'https://ark-servers.net/api/?object=servers&element=detail&key=UGkfgK5PNgpdgwGmGHKGcUgflPIjqIOE9M2';
+var mainserver = 'https://ark-servers.net/api/?object=servers&element=detail&key=UGkfgK5PNgpdgwGmGHKGcUgflPIjqIOE9M2';
+var secondserver = 'https://ark-servers.net/api/?object=servers&element=detail&key=afmC096pkzTMAL8WDM0HOWpBtpjJTodqs';
 
 var error = 'unknown';
 
 var classes = {
     error: "fa-question",
     0: "fa-times",
-    1: "fa-check",
+    1: "fa-check"
 };
 
 var allclasses = "";
 
-for(i in classes) {
-    allclasses += ' '+classes[i];
-};
+for (i in classes) {
+    allclasses += ' ' + classes[i];
+}
 
-function q(addr, cb) {
+function ParseAndDisplay(Display, Url) {
     $.ajax({
-        url: rq,
+        url: Url,
         type: 'GET',
-        dataType: 'json',
-        data: {ip: addr, players: true},
+        dataType: 'json'
     })
 
-    .done(function(data) {
-        console.log(data);
-        cb(data);
-    })
+        .done(function (data) {
+            console.log(data);
+            Display(data);
+        })
 
-    .fail(function(data) {
-        console.log("error");
-    })
+        .fail(function (data) {
+            console.log("error");
+        })
 
-    .always(function() {
-    });
+        .always(function () {
+        });
 }
 
 function setclass(o, c) {
@@ -46,33 +46,53 @@ function settext(o, t) {
     o.html(t);
 }
 
-function display(data) {
+function DisplayData(data) {
     var
-        name = $('#name'),
-        is_online = $('#online'),
-        players = $('#players'),
+        is_online =  $('#is_online'),
+        players =    $('#players'),
         maxplayers = $('#maxplayers'),
-        version = $('#version'),
-        map = $('#map'),
-        lastcheck = $('#last_check')
-        setclass(is_online, data.is_online);
-
-    if (data.is_online) {
-        settext(name, data.name);
-        settext(players, data.players);
-        settext(maxplayers, data.maxplayers);
-        settext(version, data.version);
-        settext(map, data.map);
-        settext(last_check, data.last_check);
-
+        version =    $('#version'),
+        map =        $('#map'),
+        lastcheck =  $('#last_check'),
+        updated =    $('#updated'),
+        hostname =   $('#hostname'),
+        address =    $('#address'),
+        query_port = $('#query_port');
+    //setclass(is_online, data.is_online);
+    var table = document.getElementById("statusTable");
+    console.log(data.is_online);
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    if (data.is_online === "1") {
+        /* Add the data with "server online" stuff */
+        cell1.innerHTML = data.hostname.replace(" - (v" + data.version + ")", "");
+        console.log(data.is_online.replace("1", '<i class="fab fa-asymmetrik"></i>'));
+        cell2.innerHTML = data.is_online.replace("1", '<i class="fa fa-check"></i>');
+        cell3.innerHTML = data.players + "/" + data.maxplayers;
+        cell4.innerHTML = data.map;
+        cell5.innerHTML = data.version;
+        cell6.innerHTML = '<a href="steam://connect/' + data.address + ':' + data.query_port + '">Connect</a>';
+        cell6.id = "noborder";
     }
 
     else {
-        setclass(name, error);
-        setclass(players, error);
-        setclass(maxplayers, error);
-        setclass(version, error);
-        setclass(map, error);
-        setclass(last_check, error);
+        /* Add the data with "server offline" stuff */
+        cell1.innerHTML = data.hostname.replace(" - (v" + data.version + ")", "");
+        console.log(data.is_online.replace("1", '<i class="fab fa-asymmetrik"></i>'));
+        cell2.innerHTML = data.is_online.replace("1", '<i class="fa fa-times"></i>');
+        cell3.innerHTML = data.players + "/" + data.maxplayers;
+        cell4.innerHTML = data.map;
+        cell5.innerHTML = data.version;
+        cell6.innerHTML = '(Offline)';
+        cell6.id = "noborder";
     }
 }
+$(document).ready(function () {
+    ParseAndDisplay(DisplayData, mainserver);
+    ParseAndDisplay(DisplayData, secondserver);
+});
