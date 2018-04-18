8f32c45ae6dbea5e8e2b0fefda22240e67eb345c
GAsplund <gasplund2@gmail.com>
2018-04-18 20:48:22 +0200

    Optimized code"

--- files/ark-server-status.js
+++ files/ark-server-status.js
@@ -1,24 +1,30 @@
-var mainserver = 'https://use.gameapis.net/ark/query/info/gasplund.mc-server.net:7777';
-var secondserver = 'https://use.gameapis.net/ark/query/info/gasplund.mc-server.net:7778';
+var servers = ['gasplund.mc-server.net:7777', 'gasplund.mc-server.net:7778'];
 
-function ParseAndDisplay(Url) {
+function ParseAndDisplay() {
+    var serverslist = servers[0];
+    for (i = 1; i + 1 <= servers.length; i++) {
+        console.log("Doing operation " + i + " out of " + servers.length);
+        serverslist = serverslist + "," + servers[i];
+    }
     $.ajax({
-        url: Url,
+        url: "https://use.gameapis.net/ark/query/info/" + serverslist,
         type: 'GET',
         dataType: 'json'
     })
 
-        .done(function (data) {
+        .done(function (data) /* I guess this is where data comes from*/ {
             console.log(data);
-            AddDataToTable(data);
+            for (i = 0; i + 1 <= servers.length; i++) {
+                AddDataToTable(data[servers[i]]);
+            }
+
         })
 
         .fail(function (data) {
             console.log("error");
         })
 
-        .always(function () {
-        });
+        .always(function () { });
 }
 
 function AddDataToTable(data) {
@@ -35,7 +41,7 @@ function AddDataToTable(data) {
         version = versiontemp[versiontemp.length - 1].replace(")", "");
 
     var table = document.getElementById("statusTable");
-        row = table.insertRow(table.rows.length),
+    row = table.insertRow(table.rows.length),
         cell1 = row.insertCell(0),
         cell2 = row.insertCell(1),
         cell3 = row.insertCell(2),
@@ -54,9 +60,7 @@ function AddDataToTable(data) {
         cell2.innerHTML = '<i style="font-size:22px" class="fas fa-check"></i>';
         cell6.innerHTML = '<a href="' + steamlink + '"><i style="font-size:24px" class="fas fa-sign-in-alt"></i></a>';
         cell6.id = "noborder";
-    }
-
-    else {
+    } else {
         // Add the data that is dependent on online/offline for OFFLINE STATUS
         cell2.innerHTML = '<i style="font-size:22px" class="fas fa-times"></i>';
         cell6.innerHTML = '(Offline)';
@@ -65,6 +69,5 @@ function AddDataToTable(data) {
 }
 
 $(document).ready(function () {
-    ParseAndDisplay(mainserver);
-    ParseAndDisplay(secondserver);
+    ParseAndDisplay(servers);
 });
