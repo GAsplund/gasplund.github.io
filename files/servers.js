3a74fc656ce57952f4f1d01868d8e3d63fdc41a5
xAphex <5391237+xAphex@users.noreply.github.com>
2018-04-19 19:26:29 +0300

    New Script

---
+++ files/servers.js
@@ -0,0 +1,127 @@
+var ARKservers = ['gasplund.mc-server.net:7777', 'gasplund.mc-server.net:7778'];
+var FTBservers = ['gasplund.mc-server.net'];
+
+function ParseAndDisplay(game) {
+    var ARKserverslist = ARKservers[0];
+    for (i = 1; i + 1 <= ARKservers.length; i++) {
+        ARKserverslist = ARKserverslist + "," + ARKservers[i];
+    }
+
+    var FTBserverslist = ARKservers[0];
+    for (i = 1; i + 1 <= ARKservers.length; i++) {
+        FTBserverslist = FTBserverslist + "," + ARKservers[i];
+    }
+
+	if (game === "ARK") {
+    $.ajax({
+        url: "https://use.gameapis.net/ark/query/info/" + ARKservers,
+        type: 'GET',
+        dataType: 'json'
+        })
+
+        // I guess this is where data comes from
+        .done(function (data) {
+            for (i = 0; i + 1 <= ARKservers.length; i++) {
+                AddDataToTable(data[ARKservers[i]], game);
+            }
+        })
+	} else if (game === "FTB")
+    {
+        console.log("https://use.gameapis.net/mc/query/info/" + FTBservers[0]);
+		$.ajax({
+        url: "https://use.gameapis.net/mc/query/info/" + FTBservers[0],
+        type: 'GET',
+        dataType: 'json'
+        })
+            .done(function (data) {
+                    AddDataToTable(data, game);
+            })
+    }
+
+}
+
+function AddDataToTable(data, game) {
+	if (game === "ARK") {
+		
+		//
+		//   ADD THE SERVER DATA FOR ARK: SURVIVAL EVOLVED SERVER(S)
+		//
+		
+    var status = data.status,
+        players = data.players.online,
+        maxplayers = data.players.max,
+        map = data.map,
+        name = data.name,
+        query_port = data.queryPort,
+        steamlink = data.join,
+        versiontemp = name.split(" - (v"),
+        version = versiontemp[versiontemp.length - 1].replace(")", "");
+
+    var table = document.getElementById("ARKTable");
+    row = table.insertRow(table.rows.length),
+        cell1 = row.insertCell(0),
+        cell2 = row.insertCell(1),
+        cell3 = row.insertCell(2),
+        cell4 = row.insertCell(3),
+        cell5 = row.insertCell(4),
+        cell6 = row.insertCell(5);
+
+    // Add data that is independent of online/offline
+    cell1.innerHTML = name.replace(" - (v" + version + ")", "");
+    cell3.innerHTML = players + "/" + maxplayers;
+    cell4.innerHTML = map.replace("TheIsland", "The Island");
+    cell5.innerHTML = version;
+    cell6.id = "noborder";
+
+    if (status === true) {
+        // Add the data that is dependent on online/offline for ONLINE STATUS
+        cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
+        cell6.innerHTML = '<a href="' + steamlink + '"><i class="fas fa-sign-in-alt fa-lg"></i></a>';
+    }
+
+    else {
+        // Add the data that is dependent on online/offline for OFFLINE STATUS
+        cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
+        cell6.innerHTML = '(Offline)';
+    }
+	} else if (game === "FTB")
+	{
+		
+		//
+		//   ADD THE SERVER DATA FOR FEED THE BEAST SERVER
+		//
+    var status = data.status,
+        players = data.players.online,
+        maxplayers = data.players.max,
+        hostname = data.hostname,
+        version = data.version;
+
+    var table = document.getElementById("FTBTable");
+    row = table.insertRow(table.rows.length),
+        cell1 = row.insertCell(0),
+        cell2 = row.insertCell(1),
+        cell3 = row.insertCell(2),
+        cell4 = row.insertCell(3);
+
+    // Add data that is independent of online/offline
+    cell1.innerHTML = hostname;
+    cell3.innerHTML = players + "/" + maxplayers;
+    cell4.innerHTML = version;
+	cell4.id = "noborder";
+
+    if (status === true) {
+        // Add the data that is dependent on online/offline for ONLINE STATUS
+        cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
+    }
+
+    else {
+        // Add the data that is dependent on online/offline for OFFLINE STATUS
+        cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
+    }
+	}
+}
+
+$(document).ready(function () {
+    ParseAndDisplay("ARK");
+	ParseAndDisplay("FTB");
+});
