96741d5aa3ee15126faaf85ed84ebb0d8fa522eb
xAphex <5391237+xAphex@users.noreply.github.com>
2018-04-19 13:30:16 +0300

    Update ARK Server Checker (test 2)

--- files/ark-server-status.js
+++ files/ark-server-status.js
@@ -43,12 +43,12 @@ function AddDataToTable(data) {
 
     var table = document.getElementById("statusTable");
     row = table.insertRow(table.rows.length),
-        cell1 = row.insertCell(1),
-        cell2 = row.insertCell(2),
-        cell3 = row.insertCell(3),
-        cell4 = row.insertCell(4),
-        cell5 = row.insertCell(5),
-        cell6 = row.insertCell(6);
+        cell1 = row.insertCell(0),
+        cell2 = row.insertCell(1),
+        cell3 = row.insertCell(2),
+        cell4 = row.insertCell(3),
+        cell5 = row.insertCell(4),
+        cell6 = row.insertCell(5);
 
     // Add data that is independent of online/offline
     cell1.innerHTML = hostname.replace(" - (v" + version + ")", "");
