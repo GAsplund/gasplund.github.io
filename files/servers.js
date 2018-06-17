4484fa39fe2742d1dc18b2f231b5f4f14db891e5
GAsplund <gasplund2@gmail.com>
2018-06-17 23:55:50 +0200

    Now shows servers, even if they are offline.
    Can also add one server and up, where it was only able to add two servers and up.

--- files/servers.js
+++ files/servers.js
@@ -1,6 +1,6 @@
 var CurrentlyCountingDown = true;
-var ARKservers = ['gasplund.mc-server.net:7777', 'gasplund.mc-server.net:7778'];
-var FTBservers = ['gasplund.mc-server.net:25566'];
+var ARKservers = ["gasplund.mc-server.net:7777", "gasplund.mc-server.net:7770"];
+var FTBservers = ['gasplund.mc-server.net:25565', "gasplund.mc-server.net:25566"];
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
@@ -23,128 +23,155 @@ async function RefreshCountdown() {
 function ParseAndDisplay(game) {
 
 	var ARKserverslist = ARKservers[0];
+	if (ARKservers.length >= 2) {
 	for (i = 1; i + 1 <= ARKservers.length; i++) {
 		ARKserverslist = ARKserverslist + "," + ARKservers[i];
 	}
+	}
+	
 
-	var FTBserverslist = ARKservers[0];
-	for (i = 1; i + 1 <= ARKservers.length; i++) {
+	var FTBserverslist = FTBservers[0];
+	if (FTBservers.length >= 2) {
+	for (i = 1; i + 1 <= FTBservers.length; i++) {
 		FTBserverslist = FTBserverslist + "," + ARKservers[i];
+	}
 	}
 
 	if (game === "ARK") {
 		$.ajax({
-			url: "https://use.gameapis.net/ark/query/info/" + ARKservers,
+			url: "https://use.gameapis.net/ark/query/info/" + ARKserverslist,
 			type: 'GET',
 			dataType: 'json'
 		})
 
 		// I guess this is where data comes from
 		.done(function (data) {
+			if (ARKservers.length === 1){
+				AddDataToTable(data, game);
+			} else {
 			for (i = 0; i + 1 <= ARKservers.length; i++) {
 				AddDataToTable(data[ARKservers[i]], game);
+			}
 			}
 		});
 	}
 
 	else if (game === "FTB") {
 		$.ajax({
-			url: "https://use.gameapis.net/mc/query/info/" + FTBservers[0],
+			url: "https://use.gameapis.net/mc/query/info/" + FTBserverslist,
 			type: 'GET',
 			dataType: 'json'
 		})
 
 		.done(function (data) {
-			AddDataToTable(data, game);
+			if (FTBservers.length === 1){
+				AddDataToTable(data, game);
+			} else {
+			for (i = 0; i + 1 <= FTBservers.length; i++) {
+				AddDataToTable(data[FTBservers[i]], game);
+			}
+			}
 		});
 	}
 
 }
 
-function AddDataToTable(data, game) {
+function AddDataToTable(indata, game) {
 
-	if (game === "ARK") {
+    if (game === "ARK") {
 
-		var status,
-			players,
-			maxplayers,
-			table;
+        var status,
+            players,
+            maxplayers,
+            table;
 
-		//   ADD THE SERVER DATA FOR ARK: SURVIVAL EVOLVED SERVER(S)
+        //   ADD THE SERVER DATA FOR ARK: SURVIVAL EVOLVED SERVER(S)
 
-		status = data.status;
-		players = data.players.online;
-		maxplayers = data.players.max;
-		name = data.name;
-		var query_port = data.queryPort,
-		map = data.map,
-		steamlink = data.join,
-		versiontemp = name.split(" - (v"),
-		version = versiontemp[versiontemp.length - 1].replace(")", "");
+        status = indata.status;
+        if (status) {
+            players = indata.players.online;
+            maxplayers = indata.players.max;
+            name = indata.name;
+            var query_port = indata.queryPort,
+                map = indata.map,
+                steamlink = indata.join,
+                versiontemp = indata.name.split(" - (v"),
+            version = versiontemp[versiontemp.length - 1].replace(")", "");
+        }
 
-		table = document.getElementById("ARKTable");
-		row = table.insertRow(table.rows.length),
-		cell1 = row.insertCell(0),
-		cell2 = row.insertCell(1),
-		cell3 = row.insertCell(2),
-		cell4 = row.insertCell(3),
-		cell5 = row.insertCell(4),
-		cell6 = row.insertCell(5);
+        table = document.getElementById("ARKTable");
+        row = table.insertRow(table.rows.length),
+            cell1 = row.insertCell(0),
+            cell2 = row.insertCell(1),
+            cell3 = row.insertCell(2),
+            cell4 = row.insertCell(3),
+            cell5 = row.insertCell(4),
+            cell6 = row.insertCell(5);
 
-		// Add data that is independent of online/offline
-		cell1.innerHTML = name.replace(" - (v" + version + ")", "");
-		cell3.innerHTML = players + "/" + maxplayers;
-		cell4.innerHTML = map.replace("TheIsland", "The Island");
-		cell5.innerHTML = version;
-		cell6.className += "noborder";
+        // Add data that is independent of online/offline
+        cell6.className += "noborder";
 
-		if (status === true) {
-			// Add the data that is dependent on online/offline for ONLINE STATUS
-			cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
-			cell6.innerHTML = '<a href="' + steamlink + '"><i class="fas fa-sign-in-alt fa-lg"></i></a>';
-		}
+        if (status === true) {
+            // Add the data that is dependent on online/offline for ONLINE STATUS
+            cell1.innerHTML = name.replace(" - (v" + version + ")", "");
+            cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
+            cell3.innerHTML = players + "/" + maxplayers;
+            cell4.innerHTML = map.replace("TheIsland", "The Island").replace("ScorchedEarth", "Scorched Earth");
+            cell5.innerHTML = version;
+            cell6.innerHTML = '<a href="' + steamlink + '"><i class="fas fa-sign-in-alt fa-lg"></i></a>';
+        }
 
-		else {
-			// Add the data that is dependent on online/offline for OFFLINE STATUS
-			cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
-			cell6.innerHTML = '(Offline)';
-		}
+        else {
+            // Add the data that is dependent on online/offline for OFFLINE STATUS
+            cell1.innerHTML = "?";
+            cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
+            cell3.innerHTML = "0/0";
+            cell4.innerHTML = "?";
+            cell5.innerHTML = "0.0";
+            cell6.innerHTML = '(Offline)';
+        }
 
-	}
+    }
 
-	else if (game === "FTB") {
+    else if (game === "FTB") {
 
-		// ADD THE SERVER DATA FOR FEED THE BEAST SERVER
-		status = data.status;
-		players = data.players.online;
-		maxplayers = data.players.max;
-		var name = data.motds.html;
-		version = data.version;
+        // ADD THE SERVER DATA FOR FEED THE BEAST SERVER
+        status = status;
+        if (status) {
+            players = players.online;
+            maxplayers = players.max;
+            var name = motds.html;
+            version = version;
+        }
 
-		table = document.getElementById("FTBTable");
-		row = table.insertRow(table.rows.length),
-		cell1 = row.insertCell(0),
-		cell2 = row.insertCell(1),
-		cell3 = row.insertCell(2),
-		cell4 = row.insertCell(3);
+        table = document.getElementById("FTBTable");
+        row = table.insertRow(table.rows.length),
+            cell1 = row.insertCell(0),
+            cell2 = row.insertCell(1),
+            cell3 = row.insertCell(2),
+            cell4 = row.insertCell(3);
 
-		// Add data that is independent of online/offline
-		cell1.innerHTML = name.replace("\n", "<br>");
-		cell3.innerHTML = players + "/" + maxplayers;
-		cell4.innerHTML = version;
-		cell4.className += "noborder";
+        // Add data that is independent of online/offline
+        cell4.className += "noborder";
 
-		if (status === true) {
-			// Add the data that is dependent on online/offline for ONLINE STATUS
-			cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
-		}
+        if (status === true) {
+            // Add the data corresponding to ONLINE STATUS
+            cell1.innerHTML = name.replace("\n", "<br>");
+            cell2.innerHTML = '<i class="fas fa-check fa-lg"></i>';
+            cell3.innerHTML = players + "/" + maxplayers;
+            cell4.innerHTML = version;
 
-		else {
-			// Add the data that is dependent on online/offline for OFFLINE STATUS
-			cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
-		}
+        }
 
-	}
+        else {
+            // Add the data corresponding to OFFLINE STATUS
+            cell1.innerHTML = "?";
+            cell2.innerHTML = '<i class="fas fa-times fa-lg"></i>';
+            cell3.innerHTML = "0/0";
+            cell4.innerHTML = "?";
+        }
+
+    }
 
 }
 
